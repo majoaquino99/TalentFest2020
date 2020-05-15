4c1bc9fb739bb680b4a368d36ba0f40f9e13356f
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-05-15 00:57:16 -0500

    task is active and timer view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ViewTask from './views/ViewTask';
 
 function App() {
 	return (
-		<div className="App">
-			
+		<div className="LandingView">
+			<ViewTask/>
 		</div>
 	);
 }
