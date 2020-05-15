97a1cd4c479e65626d503e86aeeba82f77f50300
majoaquino99 <majo.aqh@gmail.com>
2020-05-15 00:00:39 -0500

    Added Graphic Component

---
+++ src/views/GraphicView.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Graphic from '../components/GraphicContainer/GraphicComponent';
+
+const GraphicView = () => {
+	return(
+		<>
+			<Graphic/>
+		</>
+	)
+
+};
+
+export default GraphicView;
